2d5dceae6be4497c3666983485fe576775b87829
= <=>
2024-04-12 09:42:59 +0200

    Remove unused

--- variables.tf
+++ variables.tf
@@ -72,11 +72,6 @@ variable "management_resources_tags" {
   }
 }
 
-variable "decommissioned_subscription_ids" {
-  type    = list(string)
-  default = [""]
-}
-
 /* # Site-Site VPN
 variable "local_network_gateway_address" {
   type        = string
