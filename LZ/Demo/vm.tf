2100d7ad85b1f9488681c657ae0a6ba22c34c835
Fredrik Wikestad <fredrik.wikestad@atea.no>
2024-04-18 09:21:01 +0200

    Update to use public ip

--- LZ/Demo/vm.tf
+++ LZ/Demo/vm.tf
@@ -10,4 +10,5 @@ module "vm" {
   location            = var.resource_location
   subnet_id           = module.network.subnets["snet01"].id
   vm_password         = "ChangeMe123!"
+  use_public_ip       = true
 }
