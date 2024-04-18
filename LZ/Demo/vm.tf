c853f8cd8faecb1d0d68c31560d15035018aad80
Fredrik Wikestad <fredrik.wikestad@atea.no>
2024-04-18 08:40:00 +0200

    test vm

---
+++ LZ/Demo/vm.tf
@@ -0,0 +1,13 @@
+resource "azurerm_resource_group" "vm" {
+  name = "rg-test"
+  location = var.resource_location
+}
+
+module "vm" {
+    source = "git::https://github.com/fwikestad/terraform-azurerm-windows-vm"
+    vm_name = "test"
+    resource_group_name = azurerm_resource_group.vm.name
+    location = var.resource_location
+    subnet_id = module.network.subnets[snet_01].id
+    vm_password = "ChangeMe123!"
+}
