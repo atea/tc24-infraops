59f3c9f2b70d8c556075a6f2b0f0ff0824631c56
Fredrik Wikestad <fredrik.wikestad@atea.no>
2024-04-18 08:55:46 +0200

    Fix refUpdate ref

--- LZ/Demo/vm.tf
+++ LZ/Demo/vm.tf
@@ -8,6 +8,6 @@ module "vm" {
   vm_name             = "test"
   resource_group_name = azurerm_resource_group.vm.name
   location            = var.resource_location
-  subnet_id           = module.network.subnets[snet_01].id
+  subnet_id           = module.network.subnets["snet01"].id
   vm_password         = "ChangeMe123!"
 }
